6e37bf90ce29a2424163b8c9285b4138d4af8299
n00978 <n00978@cybird-52863.local>
2016-05-13 22:08:31 +0900

    ひっぱりおかしくなったのでここで実験

--- Assets/scripts/lobby/RoomListController.cs
+++ Assets/scripts/lobby/RoomListController.cs
@@ -27,9 +27,9 @@ public class RoomListController : MonoBehaviour {
 	private NetworkMatch networkMatch;
 	private NetworkManager networkManager;
 
-	private bool contentScrollStop = false; //true=止まってる　false=初期状態
+	public bool contentScrollStop = false; //true=止まってる　false=初期状態
 	private int defaultRoomListScrollViewHeight = -90;
-	private bool startListMatcheFlag = true; //ListMatcheの通信してもよいフラグ
+	public bool startListMatcheFlag = true; //ListMatcheの通信してもよいフラグ
 
 	//プロパティ群
 	RectTransform _roomListContentRect;
@@ -77,87 +77,93 @@ public class RoomListController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(loadIcon.activeSelf){
-			loadIcon.transform.eulerAngles += new Vector3 (0f, 0f, -1f);
+			loadIcon.transform.eulerAngles += new Vector3 (0f, 0f, -3f);
 		}
-			
-		touchNow = Input.GetMouseButton (0);
 	}
 
 	public void OnChangedScrollPosition(Vector2 position){
-		if (roomListContentRect.anchoredPosition.y < 0.0f && roomListScrollViewRectTransform.anchoredPosition.y > -240) {
+		if (roomListContentRect.anchoredPosition.y < 0.0f && roomListScrollViewRectTransform.anchoredPosition.y > -240 && !contentScrollStop) {
+			//これなんだっけ
 			roomListScrollRect.elasticity = 0.01f;
-
-			roomListScrollViewRectTransform.anchoredPosition = new Vector2 (0, roomListContentRect.anchoredPosition.y + defaultRoomListScrollViewHeight);
-			nodeGroupRectTransform.anchoredPosition = new Vector2 (0, -roomListContentRect.anchoredPosition.y * 0.9f);
-
-		} else if(roomListScrollViewRectTransform.anchoredPosition.y < -240 && !contentScrollStop) {
+			//自動で戻ってる時は追従させない
+			if(!pullBackScrollAnimationFlag){
+				//ContentにScrollRectを追従させる
+				roomListScrollViewRectTransform.anchoredPosition = new Vector2 (0, roomListContentRect.anchoredPosition.y + defaultRoomListScrollViewHeight);
+			}
+			//nodeをContentが移動した分だけ反対に移動させて子の追従を打ち消す
+			nodeGroupRectTransform.anchoredPosition = new Vector2 (0, -roomListContentRect.anchoredPosition.y * 0.99f);
+		} else if(roomListScrollViewRectTransform.anchoredPosition.y < -240 && !contentScrollStop && startListMatcheFlag) {
+			Debug.Log ("contentScrollStop");
 			contentScrollStop = true;
-			startListMatcheFlag = true;
+			//startListMatcheFlag = true;
 			roomListScrollViewRectTransform.anchoredPosition = new Vector2 (0, -240);
 			roomListScrollRect.elasticity = 0.1f;
 		}
 
-		//引っ張って話して天井までいってから通信を始める
+		//引っ張ってはなして天井までいってから通信を始める
 		if(contentScrollStop && startListMatcheFlag){
-			if(roomListContentRect.anchoredPosition.y >= -nodeGroupRectTransform.anchoredPosition.y-1){
+			if(roomListContentRect.anchoredPosition.y >= -nodeGroupRectTransform.anchoredPosition.y - 1){
 				startListMatcheFlag = false;
 
 				//矢印とローディング
 				loadArrowIcon.SetActive(false);
 				loadIcon.SetActive(true);
 
-				networkMatch.ListMatches(0, 20, /*"{"+rank+"}"*/"", ListMatcheCallBack);
+				//networkMatch.ListMatches(0, 20, /*"{"+rank+"}"*/"", ListMatcheCallBack);
+				StartCoroutine(PullBackScrollView(null));
 			}
+		}
+
+
+		if(roomListScrollViewRectTransform.anchoredPosition.y >= -90-1 && !startListMatcheFlag){
+			Debug.Log ("End");
+			startListMatcheFlag = true;
 		}
 	}
 
 	private void ListMatcheCallBack(ListMatchResponse matchList){
+		Debug.Log ("ListMatcheCallBack");
 		StartCoroutine(PullBackScrollView(matchList));
 	}
 
-	[SerializeField, Range(0, 10)]
-	float time = 0.1f;
-	private bool touchNow;
-	private float startTime;
 
+	private bool pullBackScrollAnimationFlag = false;
 	public IEnumerator PullBackScrollView(ListMatchResponse matchList){
-		//if(startListMatcheFlag){
-			startListMatcheFlag = false;
-			
-			yield return new WaitForSeconds(1);
-			Debug.Log ("PullBackScrollView"+startListMatcheFlag);
-			startTime = Time.timeSinceLevelLoad;
+		startListMatcheFlag = false;
+		Debug.Log ("XXXXXXXXXXXXXXXX");
+		yield return new WaitForSeconds(1);
+		Debug.Log ("PullBackScrollView"+startListMatcheFlag);
 
-			//GUIで表示メソッド実行
-			if(matchList != null){
-				roomListContent.GetComponent<GenMatchListController>().GenMatchList (matchList);
-			}
+		//GUIで表示メソッド実行
+		if(matchList != null){
+			roomListContent.GetComponent<GenMatchListController>().GenMatchList (matchList);
+		}
 
-			//戻るアニメーション
-			bool pullBackScrollAnimationFlag = true;
-			while (pullBackScrollAnimationFlag) {
-				//タップ中ならアニメーションさせない
-				if ((Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) || Input.GetMouseButton (0)) {
-					startTime = Time.timeSinceLevelLoad;
-					yield return null;
-				}else {
-					var diff = Time.timeSinceLevelLoad - startTime;
-					var rate = diff / time;
-
-					roomListScrollViewRectTransform.anchoredPosition = Vector2.Lerp (roomListScrollViewRectTransform.anchoredPosition, new Vector3 (0, defaultRoomListScrollViewHeight, 0), rate);
-					if (roomListScrollViewRectTransform.anchoredPosition.y >= defaultRoomListScrollViewHeight - 1) {
-						pullBackScrollAnimationFlag = false;
-						contentScrollStop = false;
-					}
-					yield return null;
+		//戻るアニメーション
+		pullBackScrollAnimationFlag = true;
+		while (pullBackScrollAnimationFlag) {
+			//タップ中ならアニメーションさせない
+			if ((Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) || Input.GetMouseButton (0)) {
+				yield return null;
+			} else {
+				float rate = 0.1f;
+				contentScrollStop = false;
+				roomListScrollViewRectTransform.anchoredPosition = Vector2.Lerp (roomListScrollViewRectTransform.anchoredPosition, new Vector2 (0, defaultRoomListScrollViewHeight), rate);
+				if (roomListScrollViewRectTransform.anchoredPosition.y >= defaultRoomListScrollViewHeight - 1) {
+					pullBackScrollAnimationFlag = false;
+					contentScrollStop = false;
+					startListMatcheFlag = true;
+					Debug.Log ("Animation end");
 				}
+				Debug.Log ("BBBB");
+				yield return null;
 			}
+		}
 
-			//ひっぱって出てくる矢印とローディング
-			loadArrowIcon.SetActive(true);
-			loadIcon.SetActive(false);
+		//ひっぱって出てくる矢印とローディング
+		loadArrowIcon.SetActive(true);
+		loadIcon.SetActive(false);
 
-			yield break;
-		//}
+		yield break;
 	}
 }
